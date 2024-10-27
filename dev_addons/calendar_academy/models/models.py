2202aed08b9cdc888f9c50617e531690568e363e
NielsRoy <nielsroy8@gmail.com>
2024-10-27 18:34:52 -0400

    Materias y Cursos sin validaciones

--- dev_addons/calendar_academy/models/models.py
+++ dev_addons/calendar_academy/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
 # class calendar_academy(models.Model):
@@ -17,3 +17,27 @@
 #         for record in self:
 #             record.value2 = float(record.value) / 100
 
+class materia(models.Model):
+    _name = 'calendar_academy.materia'
+    _description = 'calendar_academy.materia'
+
+    name = fields.Char(
+        string='Nombre',
+        required=True,
+        help='Introduzca el nombre de la materia',
+        readonly=False
+    )
+    descripcion = fields.Text()
+    curso = fields.Many2one('calendar_academy.curso', ondelete='cascade', help='Curso relacionado')
+
+class curso(models.Model):
+    _name = 'calendar_academy.curso'
+    _description = 'calendar_academy.curso'
+
+    name = fields.Char(
+        string='Nombre',
+        unique=True,
+        help='Introduzca el nombre del curso',
+        readonly=False
+    )
+    materias = fields.One2many(string='Materias', comodel_name='calendar_academy.materia', inverse_name='curso')
