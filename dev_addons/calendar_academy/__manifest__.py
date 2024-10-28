e3f2dd99cd1f90e63b8162317513ecf8aad27090
jherson <rodriguezpaul98@gmail.com>
2024-10-28 00:10:34 -0400

    basic perid list

--- dev_addons/calendar_academy/__manifest__.py
+++ dev_addons/calendar_academy/__manifest__.py
@@ -14,15 +14,15 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Education',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'calendar'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
@@ -30,5 +30,7 @@ Long description of module's purpose
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True,
+    'installable': True,
 }
 
--- dev_addons/calendar_academy/models/models.py
+++ dev_addons/calendar_academy/models/models.py
@@ -1,19 +1,68 @@
-# -*- coding: utf-8 -*-
-
+# # -*- coding: utf-8 -*-
+#
 # from odoo import models, fields, api
-
-
+#
+#
 # class calendar_academy(models.Model):
 #     _name = 'calendar_academy.calendar_academy'
 #     _description = 'calendar_academy.calendar_academy'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
 #
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+#     period = fields.Char()
+# #     value = fields.Integer()
+# #     value2 = fields.Float(compute="_value_pc", store=True)
+# #     description = fields.Text()
+# #
+# #     @api.depends('value')
+# #     def _value_pc(self):
+# #         for record in self:
+# #             record.value2 = float(record.value) / 100
+#
 
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
+
+class calendar_academy(models.Model):
+    _name = 'calendar_academy.calendar_academy'
+    _description = 'calendar_academy.calendar_academy'
+    _order = 'start_date desc'
+
+    name = fields.Char(string='Nombre del Período', compute='_compute_name', store=True)
+    start_date = fields.Date(
+        string='Fecha de Inicio',
+        required=True,
+        default=fields.Date.context_today
+    )
+    end_date = fields.Date(
+        string='Fecha de Fin',
+        required=True,
+        default=lambda self: fields.Date.context_today(self) + relativedelta(months=4)
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('draft', 'Borrador'),
+        ('active', 'Activo'),
+        ('closed', 'Cerrado'),
+    ], string='Estado', default='draft', required=True)
+
+    @api.depends('start_date', 'end_date')
+    def _compute_name(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                year = record.start_date.year
+                month = record.start_date.month
+                if month <= 4:
+                    period = '1'
+                elif month <= 8:
+                    period = '2'
+                else:
+                    period = '3'
+                record.name = f'Período {year}-{period}'
+            else:
+                record.name = 'Nuevo Período'
+
+    @api.onchange('start_date')
+    def _onchange_start_date(self):
+        if self.start_date:
+            self.end_date = self.start_date + relativedelta(months=12)
